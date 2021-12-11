e9a6adaa7818562a59a0a462a40f155b365c3a6b
tdor <thomas_dorfer@gmx.net>
2021-12-11 14:08:40 +0100

    Add solution for day 11

---
+++ Aoc.D11/MatrixExtensions.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+public static class MatrixExtensions
+{
+    public static IEnumerable<Point> Adjacents<T>(this T[,] matrix, Point p)
+    {
+        bool left = p.X > 0;
+        bool top = p.Y > 0;
+        bool right = p.X < matrix.GetLength(0) - 1;
+        bool bottom = p.Y < matrix.GetLength(1) - 1;
+
+        if (left) yield return new Point(p.X - 1, p.Y);
+        if (top) yield return new Point(p.X, p.Y - 1);
+        if (right) yield return new Point(p.X + 1, p.Y);
+        if (bottom) yield return new Point(p.X, p.Y + 1);
+
+        if (left && top) yield return new Point(p.X - 1, p.Y - 1);
+        if (left && bottom) yield return new Point(p.X - 1, p.Y + 1);
+        if (right && top) yield return new Point(p.X + 1, p.Y - 1);
+        if (right && bottom) yield return new Point(p.X + 1, p.Y + 1);
+    }
+
+    public static bool IsFlashing(this int[,] matrix, Point point)
+    {
+        return matrix[point.X, point.Y] > 9;
+    }
+
+
+    public static IEnumerable<Point> Points<T>(this T[,] matrix)
+    {
+        for (int x = 0; x < matrix.GetLength(0); x++)
+        {
+            for (int y = 0; y < matrix.GetLength(1); y++)
+            {
+                yield return new Point(x, y);
+            }
+        }
+    }
+
+    public static void Increment(this int[,] matrix, IEnumerable<Point> points)
+    {
+        foreach (var point in points)
+        {
+            matrix[point.X, point.Y]++;
+        }
+    }
+
+    public static int ResetFlashed(this int[,] matrix)
+    {
+        var resets = 0;
+
+        for (int x = 0; x < matrix.GetLength(0); x++)
+        {
+            for (int y = 0; y < matrix.GetLength(1); y++)
+            {
+                if (matrix[x, y] > 9)
+                {
+                    matrix[x, y] = 0;
+                    resets++;
+                }
+            }
+        }
+
+        return resets;
+    }
+
+    public static string Print(this int[,] matrix)
+    {
+        StringBuilder sb = new();
+
+        for (int x = 0; x < matrix.GetLength(0); x++)
+        {
+            for (int y = 0; y < matrix.GetLength(1); y++)
+            {
+                string c = matrix[x, y] <= 9 ? matrix[x, y].ToString() : "+";
+                sb.Append(c);
+            }
+            sb.AppendLine();
+        }
+
+        return sb.ToString();
+    }
+}
---
+++ Aoc.D11/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Aoc D11!");
+
+var inputLines = await File.ReadAllLinesAsync("input.txt");
+int[,] matrix = new int[inputLines.Length, inputLines[0].Length];
+for (int x = 0; x < inputLines.Length; x++)
+{
+    for (int y = 0; y < inputLines[0].Length; y++)
+    {
+        matrix[x, y] = int.Parse(inputLines[x][y].ToString());
+    }
+}
+
+var flashes = 0;
+for (int step = 1; ; step++)
+{
+    matrix.Increment(matrix.Points());
+    var initialFlashes = matrix.Points().Where(matrix.IsFlashing);
+    Stack<Point> pointsToProcess = new(initialFlashes);
+    HashSet<Point> alreadyFlashedPoints = new(initialFlashes);
+
+    while (pointsToProcess.Count > 0)
+    {
+        var point = pointsToProcess.Pop();
+        var adjacents = matrix.Adjacents(point);
+        matrix.Increment(adjacents);
+
+        foreach (var adjacent in adjacents)
+        {
+            if (matrix.IsFlashing(adjacent))
+            {
+                if (alreadyFlashedPoints.Add(adjacent))
+                {
+                    pointsToProcess.Push(adjacent);
+                }
+            }
+        }
+    }
+
+    flashes += matrix.Points().Where(matrix.IsFlashing).Count();
+    var resets = matrix.ResetFlashed();
+
+    if (step == 100) Console.WriteLine($"Part 1: Total flashes after 100 steps: {flashes}");
+    if (resets == matrix.Length)
+    {
+        Console.WriteLine($"Part 2: First simultanious flashes at step {step}");
+        break;
+    }
+}
